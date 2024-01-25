aea2e64f653ec4c863210e4e3cf7a56a698ccc5d
yashi3379 <naokichi1106@gmail.com>
2024-01-25 09:50:30 +0900

    現在のコード（無限ループ解決できていない）

--- src/components/pages/Mypage.jsx
+++ src/components/pages/Mypage.jsx
@@ -2,18 +2,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useCheckSession from '../../hooks/useCheckSession';
+import { useEffect } from 'react';
 
 
 
 
 const MyPage = () => {
     const navigate = useNavigate();
-    const { user, isLoggedIn} = useCheckSession();
+    const { user, isLoggedIn } = useCheckSession();
 
+    useEffect(() => {
+        if (!isLoggedIn) {
+            navigate('/login');
+        }
+    }, [isLoggedIn, navigate]);
 
-    if (!isLoggedIn) {
-        return navigate('/login');
+    // user が undefined の場合をチェック
+    if (!user) {
+        return <p>読み込み中</p>;
     }
+    
+
+
     return (
         <div>
             <h1>マイページ</h1>
