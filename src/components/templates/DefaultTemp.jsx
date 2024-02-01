79037217f304c25388394245f1d5cd9c5494d2b1
yashi3379 <naokichi1106@gmail.com>
2024-02-01 22:17:24 +0900

    認証の修正

--- src/components/templates/DefaultTemp.jsx
+++ src/components/templates/DefaultTemp.jsx
@@ -1,18 +1,39 @@
-import React from "react";
-
+import React, { useContext } from "react";
 import { Header } from "../organisms/layout/Header";
 import { Footer } from "../organisms/layout/Footer";
+import { AuthProvider, AuthContext } from "../../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
 
+const Content = (props) => {
+    console.log(props);
+    const { children, isPublic } = props;
+    const { user } = useContext(AuthContext);
+    console.log(user);
+    console.log(isPublic);
 
-export const DefaultTemp = (props) => {
-    const { children } = props;
-   
+    if (!isPublic && !user) {
+        return <Navigate to="/login" />
+    }
 
     return (
         <>
-        <Header />
-        {children}
-        <Footer />
+            <Header />
+            {children}
+            <Footer />
+        </>
+    )
+}
+
+
+export const DefaultTemp = (props) => {
+    console.log(props);
+    const { children } = props;
+
+    return (
+        <>
+            <AuthProvider>
+                <Content>{children}</Content>
+            </AuthProvider>
         </>
     );
-    };
+};
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,31 +1,27 @@
-import React,{useContext} from 'react';
-import {Routes,Route, BrowserRouter,Navigate} from 'react-router-dom';
+import React from 'react';
+import {Routes,Route, BrowserRouter} from 'react-router-dom';
 
-import{Main} from '../components/templates/Main';
+
 import { DefaultTemp } from '../components/templates/DefaultTemp';
 
 import Mypage from '../components/pages/Mypage';
 import UserRegister from '../components/pages/UserRegister';
 import Login  from '../components/pages/Login';
 import { ServerError } from '../components/pages/errors/ServerError';
-import { AuthContext } from '../providers/AuthProvider';
+
 import CreateDiary from '../components/pages/CreateDiary';
 
 
-
-
 export const Router = () => {
-    const { user,isLoggedIn } = useContext(AuthContext);
-    console.log(user);
-    console.log(isLoggedIn);
+    
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={isLoggedIn && (user !== undefined || user !== null) ? <Main><Mypage/></Main> : <Navigate to="/login"/>} />
-                <Route path ="/create" element={isLoggedIn && (user !== undefined || user !== null) ? <DefaultTemp><CreateDiary/></DefaultTemp> : <Navigate to="/login"/>} />
-                <Route path="/register" element={!isLoggedIn || (user === undefined || user === null)  ? <DefaultTemp><UserRegister/></DefaultTemp> : <Navigate to="/"/>} />
-                <Route path="/login" element={!isLoggedIn || (user === undefined || user === null) ? <DefaultTemp><Login/></DefaultTemp> : <Navigate to="/"/>} />
-                <Route path="/500" element={<DefaultTemp><ServerError/></DefaultTemp>} />
+                <Route path="/" element= {<DefaultTemp><Mypage/></DefaultTemp>}  />
+                <Route path ="/create" element={<DefaultTemp><CreateDiary/></DefaultTemp>} />
+                <Route path="/register" element={ <DefaultTemp isPublic><UserRegister/></DefaultTemp> } />
+                <Route path="/login" element={ <DefaultTemp isPublic><Login/></DefaultTemp>} />
+                <Route path="/500" element={<DefaultTemp isPublic><ServerError/></DefaultTemp>} />
             </Routes>
         </BrowserRouter>
     )
